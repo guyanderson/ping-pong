86269b1a41bef3b386c8ef7c4fc00366cc44fbbe
guy <luckythelich@hotmail.com>
2017-05-05 09:13:41 -0700

    This will create a function to take userNumber and return user a value, currently the value is userNumber.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,18 @@
+function getInput(input){
+return(input);
+};
+
+
+
+
 $(document).ready(function(){
   $("form#userInput").submit(function(event) {
   event.preventDefault();
 
   var userNumber = parseInt($("#userNumberInput").val());
-  $("ul").append("<li>"+userNumber+"</li>");
-  // alert(userNumber);
+  var result = getInput(userNumber);
+  // $("ul").append("<li>"+userNumber+"</li>");
+  alert(result);
 
   });
 });
