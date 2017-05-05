e0a44a7ced599678c34996987f065904d923c6b0
guy <luckythelich@hotmail.com>
2017-05-05 10:51:59 -0700

    This will remove obsolete code, fix indentation.

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,23 @@
-var array = [];
 function getInput(input){
   for(var i = 1; i <= input; i++) {
-    if(i % 15 === 0) {
+    if (i % 15 === 0) {
       j = "Ping-Pong!"
-    }
-      else if(i % 5 === 0) {
+    } else if (i % 5 === 0) {
         j = "Pong!"
-      }
-      else if(i % 3 === 0) {
+    } else if (i % 3 === 0) {
         j = "Ping!"
-      }
-      else {
+    } else {
         j = i
-      }
+    }
       $("ul").append("<li>"+j+"</li>");
-    array.push(j)
-
   }
-  return array;
 };
-
-
 
 
 $(document).ready(function(){
   $("form#userInput").submit(function(event) {
   event.preventDefault();
-
   var userNumber = parseInt($("#userNumberInput").val());
   var result = getInput(userNumber);
-  $("ul").append("<li>"+result+"<br/></li>");
-
-
   });
 });
